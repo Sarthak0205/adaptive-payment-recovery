e2618cbbb09aff570c068b67d2d0e1d6f039b70b
Sarthak Chaudhari <codeSarthak02@gmail.com>
2026-09-05 08:51:47 +0530

    Allow deployed frontend origin

--- api/main.py
+++ api/main.py
@@ -13,9 +13,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://adaptive-payment-recovery-1.onrender.com",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
